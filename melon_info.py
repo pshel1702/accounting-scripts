5157d80faed0f2170db7a8f345c07b0dc55c5efa
Prachi Shelgikar <praachi.shelgikar@gmail.com>
2021-03-30 16:49:10 +0000

    Refactored code to save data in a dictionary

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,17 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melons
 
+def print_all_melons(melons):
+    """Print each melon with corresponding data"""
 
-def print_melon(name, seedless, price):
-    """Print each melon with corresponding attribute information."""
+    for melon_name,attribute in melons.items():
+        print(melon_name.upper())
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+        for attribute, value in attribute.items():
+            print(f'\t{attribute}:{value}')
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+    
 
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+print_all_melons(melons)
